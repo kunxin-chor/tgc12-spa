03ad77955b91b57d98d24575be2d3e04478a5258
kunxin-chor <chorkunxin@yahoo.com>
2021-05-11 05:59:46 +0000

    Finished the transition example

---
+++ 01-transitions/script.js
@@ -0,0 +1,13 @@
+document.querySelector('#animate-btn').addEventListener('click', function(){
+    // add the stretch class to the div with the id of thebox
+    document.querySelector('#thebox').classList.add('stretch')
+})
+
+// setTimeout(function(){
+//      document.querySelector('#thebox').classList.add('stretch')
+// }, 100)
+
+window.addEventListener('DOMContentLoaded', function(){
+     document.querySelector('#thebox').classList.add('stretch')
+
+})
